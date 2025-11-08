9d3910f6f85019efa25e0728ca60382686ae34f5
horishota6566 <shou6130@gmail.com>
2025-11-08 16:53:16 +0900

    fizzbuzzプログラムを作成

---
+++ 01.fizzbuzz/fizz_buzz.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+function fizzBuzz(n) {
+  if (n % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (n % 3 === 0) {
+    return 'Fizz';
+  } else if (n % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return `${n}`;
+  }
+}
+
+for (let n = 1; n <= 20; n++) {
+  console.log(fizzBuzz(n));
+}
