9eba33f5c205d0ab2905d665c16f1b9c8f0d1d05
horishota6566 <shou6130@gmail.com>
2025-11-12 15:08:05 +0900

    refactor: PrettierのPlaygroundで修正

--- 01.fizzbuzz/fizz_buzz.js
+++ 01.fizzbuzz/fizz_buzz.js
@@ -2,11 +2,11 @@
 
 function fizzBuzz(n) {
   if (n % 3 === 0 && n % 5 === 0) {
-    return 'FizzBuzz';
+    return "FizzBuzz";
   } else if (n % 3 === 0) {
-    return 'Fizz';
+    return "Fizz";
   } else if (n % 5 === 0) {
-    return 'Buzz';
+    return "Buzz";
   } else {
     return `${n}`;
   }
