d4fa87eb11737cdb372d4a56ada75617787ec7fa
horishota6566 <shou6130@gmail.com>
2025-11-19 11:46:19 +0900

    feat: Promise でエラーありのプログラムを実装

---
+++ 03.asynchronous/promise/error.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import { openDB, run, all } from "./common.js";
+
+openDB()
+  .then((db) => {
+    return run(
+      db,
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    );
+  })
+  .then((db) => {
+    const titles = ["本A", "本B", "本C", "本D", "本A"];
+    const sql = "INSERT INTO books (title) VALUES (?)";
+    const promises = titles.map((title) => run(db, sql, [title]));
+
+    return Promise.all(promises).then(() => db);
+  })
+  .then((db) => {
+    return all(db, "SELECT id, title, content FROM books");
+  })
+  .then((db) => {
+    return run(db, "DROP TABLE books");
+  })
+  .then((db) => {
+    db.close();
+  });
